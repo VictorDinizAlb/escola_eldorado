900f49fe05956115daf91c9c695cf207d66e5c5b
Victor Mateus <victordiniz_@outlook.com>
2022-04-08 13:14:21 -0400

    driver mysql e conexao mysql

---
+++ src/main/java/br/com/escolaeldorado/EscolaeldoradoApplication.java
@@ -0,0 +1,13 @@
+package br.com.escolaeldorado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaeldoradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaeldoradoApplication.class, args);
+	}
+
+}
