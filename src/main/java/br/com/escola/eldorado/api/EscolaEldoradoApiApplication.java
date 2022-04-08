edf1127854af96812e433ac7691cbc3c36d32b8b
Victor Mateus <victordiniz_@outlook.com>
2022-04-07 20:04:29 -0400

    Pirmeiro commit / criação do projeto com springboot

---
+++ src/main/java/br/com/escola/eldorado/api/EscolaEldoradoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.escola.eldorado.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaEldoradoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaEldoradoApiApplication.class, args);
+	}
+
+}
